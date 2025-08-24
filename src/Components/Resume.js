2ba11f1ef845dd0d67435b4677d9ef84a89524c3
Raghav Babbar <89309326+BabbarRaghav@users.noreply.github.com>
2025-08-24 14:39:02 +0000

    add experience

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -32,6 +32,21 @@ const Resume = () => {
     },
   ])
 
+  const [experience, setExperience] = useState([
+    {
+      date: "Sept 2022 - Sept 2025",
+      title: "Backend Developer",
+      name: "ReinGames Pvt Ltd",
+      desc: [
+        "Developed and maintained 20+ microservices in an agile environment, ensuring scalability, reliability, and performance.",
+        "Led backend development for popular gaming applications such as 3 Patti, Poker, and Indian Poker, delivering low-latency, high-availability systems.",
+        "Designed and implemented a custom CRM backend solution from scratch, streamlining workflows and improving system efficiency.",
+        "Proficient in Golang with expertise in building and deploying microservices architectures and containerized applications using Docker.",
+        "Collaborated with cross-functional teams, conducted code reviews, and contributed to system design and performance optimization."
+      ]
+    }
+  ])
+
   const [Community, setCommunity] = useState([
     {
       date: "March 2022",
@@ -43,31 +58,47 @@ const Resume = () => {
 
   return (
     <div className="main flex flex-col lg:flex-row justify-center items-center lg:items-baseline mt-9 text-center">
-        <div className="education lg:h-full">
-            <div className="font-bold text-2xl">Education</div>
-            {Education.map((edu)=> {
-            return ( <div className="box m-10 rounded-3xl border border-gray-200 shadow-md p-4 size">
-                <p className="text-slate-400 underline mb-2">{edu.date}</p>
-                <p className="font-bold text-left">{edu.name}</p>
-                <p className="font-bold text-left text-slate-400">{edu.title}</p>
-                <p className="text-left">{edu.place}</p>
-                <hr className="my-4" />
-                <p className="text-left">{edu.mark}</p>
-            </div>)
-            })}
-        </div>
-        <div className="comm-work lg:h-full">
-            <div className="font-bold text-2xl">Community Work</div>
-            {Community.map((comm)=>{
-            return(<div className="box m-10 rounded-3xl border border-gray-200 shadow-md p-4 lg:size">
-                <p className="text-slate-400 underline mb-2">{comm.date}</p>
-                <p className="font-bold text-left">{comm.name}</p>
-                <p className="text-left">{comm.title}</p>
-                <hr className="my-4" />
-                <p className="text-left">{comm.desc}</p>
-            </div>
-            )})}
-        </div>
+      <div className="education lg:h-full">
+        <div className="font-bold text-2xl">Education</div>
+        {Education.map((edu) => {
+          return (<div className="box m-10 rounded-3xl border border-gray-200 shadow-md p-4 size">
+            <p className="text-slate-400 underline mb-2">{edu.date}</p>
+            <p className="font-bold text-left">{edu.name}</p>
+            <p className="font-bold text-left text-slate-400">{edu.title}</p>
+            <p className="text-left">{edu.place}</p>
+            <hr className="my-4" />
+            <p className="text-left">{edu.mark}</p>
+          </div>)
+        })}
+      </div>
+      <div className="comm-work lg:h-full">
+      <div className="font-bold text-2xl">Experience</div>
+        {experience.map((exp) => {
+          return (<div className="box m-10 rounded-3xl border border-gray-200 shadow-md p-4 lg:size">
+            <p className="text-slate-400 underline mb-2">{exp.date}</p>
+            <p className="font-bold text-left">{exp.name}</p>
+            <p className="text-left">{exp.title}</p>
+            <hr className="my-4" />
+            <ul className="text-left list-disc ml-4">
+              {exp.desc.map(desc => {
+                return <li>{desc}</li>
+              })}
+            </ul>
+          </div>
+          )
+        })}
+        <div className="font-bold text-2xl">Community Work</div>
+        {Community.map((comm) => {
+          return (<div className="box m-10 rounded-3xl border border-gray-200 shadow-md p-4 lg:size">
+            <p className="text-slate-400 underline mb-2">{comm.date}</p>
+            <p className="font-bold text-left">{comm.name}</p>
+            <p className="text-left">{comm.title}</p>
+            <hr className="my-4" />
+            <p className="text-left">{comm.desc}</p>
+          </div>
+          )
+        })}
+      </div>
     </div>
   )
 }
